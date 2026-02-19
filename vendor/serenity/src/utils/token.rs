4312f371d33c6c9c9f47728d71018f166344e9bc
Nenad Nikolin <nenad@nikolin.eu>
2026-02-19 13:09:07 +0100

    chore: replace real-looking example token in serenity vendor (scanner false positive)

--- vendor/serenity/src/utils/token.rs
+++ vendor/serenity/src/utils/token.rs
@@ -17,7 +17,7 @@ use std::{fmt, str};
 /// use serenity::utils::token::validate;
 ///
 /// // ensure a valid token is in fact a valid format:
-/// assert!(validate("Mjg4NzYwMjQxMzYzODc3ODg4.C_ikow.j3VupLBuE1QWZng3TMGH0z_UAwg").is_ok());
+/// assert!(validate("EXAMPLE_ID_BASE64.TSTAMP.EXAMPLE_SIGNATURE_FAKE").is_ok());
 ///
 /// assert!(validate("Mjg4NzYwMjQxMzYzODc3ODg4").is_err());
 /// assert!(validate("").is_err());
